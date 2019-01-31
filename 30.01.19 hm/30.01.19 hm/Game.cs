d759a5eb293e08c24380d159129e759c14e43576
sagitush <44786027+sagitush@users.noreply.github.com>
2019-01-31 20:38:27 +0200

    Add files via upload

---
+++ 30.01.19 hm/30.01.19 hm/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._01._19_hm
+{
+    class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+        public Game()
+        {
+        }
+
+        public Game(string name, float rating=5.5f)
+        {
+            _name = name;
+            _rating = rating;
+        }
+
+        public Game(string name, int numberOfPlayers, float rating, string originCountry)
+        {
+            _name = name;
+            _numberOfPlayers = numberOfPlayers;
+            _rating = rating;
+            _originCountry = originCountry;
+        }
+
+        public Game(string name, string originCountry)
+        {
+            _name = name;
+            _originCountry = originCountry;
+        }
+        public int TellMeHowManyPlayers()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return $"Game:{_name},number of players:{_numberOfPlayers},rating:{_rating},origin country:{_originCountry}";
+        }
+
+    }
+}
---
+++ 30.01.19 hm/30.01.19 hm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._01._19_hm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game chess = new Game("chess", 2, 10 / 6f, "India");
+            Game sudoku = new Game()
+            {
+                _name = "sudoku",
+                _numberOfPlayers = 1,
+                _originCountry = "France",
+                _rating = 90f
+
+            };
+            Game soccer = new Game("soccer")
+            {
+                _numberOfPlayers = 22,
+                _originCountry = "England",
+
+            };
+            Game basketball = new Game("basketball", "United States")
+            {
+                _numberOfPlayers = 10,
+                _rating = 50 / 30,
+            };
+            PrintGame(soccer);
+
+        }
+        public static void PrintGame(Game g)
+        {
+            Console.WriteLine(g);
+            int number = g.TellMeHowManyPlayers();
+            Console.WriteLine($"There are:{number} players");
+        }
+    }
+}
